f56e5c1065b0971caf9a3f8f29663081f93041bb
冯梓萌 <xinuoerr@sina.com>
2018-06-20 09:02:21 +0800

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,11 +8,6 @@
 @section('content')
     <div class="banner">
         <section class="box">
-            <ul class="texts">
-                <p>打了死结的青春，捆死一颗苍白绝望的灵魂。</p>
-                <p>为自己掘一个坟墓来葬心，红尘一梦，不再追寻。</p>
-                <p>加了锁的青春，不会再因谁而推开心门。</p>
-            </ul>
         </section>
     </div>
     <div class="template">
